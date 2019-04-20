65add0bfbf55b31197b3c50580fcea2013b03f20
Nikitinho <nekitnikitin@yandex.ru>
2019-04-20 14:30:25 +0300

    PDF printing refactoring. Printing project function remains unstable.

--- src/main/resources/static/js/mixin/PrintingRiskMixin.js
+++ src/main/resources/static/js/mixin/PrintingRiskMixin.js
@@ -4,9 +4,7 @@ import JSZip from 'jszip'
 
 export default {
     methods: {
-        savePDF(risk) {
-            pdfMake.vfs = pdfFonts.pdfMake.vfs
-
+        convertToPdf(risk) {
             let content = [
                 {
                     text: `Risk id: ${risk.id}`,
@@ -21,11 +19,14 @@ export default {
             // Don't need 'id' and 'text' cause these properties are explicitly added to content array
             // Property/field 'Responsible users' needs manual parsing
             // Comments section is not included in .pdf file
+            // Notifications flag is not included in .pdf file
             const keys = Object.keys(risk).filter(key =>
                 (String)(key) !== 'id' &&
                 (String)(key) !== 'text' &&
                 (String)(key) !== 'comments' &&
-                (String)(key) !== 'responsible')
+                (String)(key) !== 'responsible' &&
+                (String)(key) !== 'areNotificationSent'
+            )
 
             keys.forEach(function (key) {
                 if (risk[key] !== null && risk[key] !== undefined && risk[key] !== '') {
@@ -49,7 +50,7 @@ export default {
                 })
             }
 
-            let result = {
+            return {
                 content,
                 styles: {
                     header: {
@@ -61,72 +62,23 @@ export default {
                     }
                 }
             }
+        },
+        savePDF(risk) {
+            pdfMake.vfs = pdfFonts.pdfMake.vfs
 
-            pdfMake.createPdf(result).download(`Risk${risk.id}`)
+            pdfMake.createPdf(this.convertToPdf(risk)).download(`Risk${risk.id}`)
         },
         async saveAllPDF(risks, projectId) {
             pdfMake.vfs = pdfFonts.pdfMake.vfs
 
             let zip = new JSZip()
             let folder = zip.folder(`Project${projectId}`);
+            let this$ = this
 
             risks.forEach(function (risk, riskIndex) {
-                let content = [
-                    {
-                        text: `Risk id: ${risk.id}`,
-                        style: 'header'
-                    },
-                    {
-                        text: `Risk title: ${risk.text}`,
-                        style: 'default'
-                    }
-                ]
-
-                // Don't need 'id' and 'text' cause these properties are explicitly added to content array
-                // Property/field 'Responsible users' needs manual parsing
-                // Comments section is not included in .pdf file
-                const keys = Object.keys(risk).filter(key =>
-                    (String)(key) !== 'id' &&
-                    (String)(key) !== 'text' &&
-                    (String)(key) !== 'comments' &&
-                    (String)(key) !== 'responsible')
-
-                keys.forEach(function (key) {
-                    if (risk[key] !== null && risk[key] !== undefined && risk[key] !== '') {
-                        let text = String(key).replace( /([A-Z])/g, " $1" );
-                        let sentenceCaseText = text.charAt(0).toUpperCase() + text.slice(1);
-                        content.push ({
-                            text: `${sentenceCaseText}: ${risk[key]}`,
-                            style: 'default'
-                        })
-                    }
-                })
-
-                let responsibleUsers = risk['responsible']
-
-                if(responsibleUsers !== null && responsibleUsers !== undefined && responsibleUsers.length > 0) {
-                    let usersEmails = []
-                    responsibleUsers.forEach(user => usersEmails.push(user.email))
-                    content.push({
-                        text: `Responsible users: ${usersEmails.join(', ')}`,
-                        style: 'default'
-                    })
-                }
-
-                let result = {
-                    content,
-                    styles: {
-                        header: {
-                            fontSize: 18,
-                            bold: true
-                        },
-                        default: {
-                            fontSize: 14
-                        }
-                    }
-                }
                 let pdfBlob = null
-                const pdfDocGenerator = pdfMake.createPdf(result)
+                let pdfFile = this$.convertToPdf(risk)
+                const pdfDocGenerator = pdfMake.createPdf(pdfFile)
 
                 pdfDocGenerator.getBlob((blob) => {
                     pdfBlob = blob
